5118ee4277c810e6537a9fbae5a24b055c93d012
Oncu <oohancan@gmail.com>
2022-08-23 12:06:46 +0300

    Initial Commit

---
+++ ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Oncu Can on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
